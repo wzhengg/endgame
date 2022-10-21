17f89e53998669b5792a1e96b92bff68aad97cce
William Zheng <williamzheng2001@gmail.com>
2022-10-20 21:36:30 -0400

    Create carousel data

---
+++ src/components/ProductCarousel/CarouselData.ts
@@ -0,0 +1,21 @@
+import Dalco959Mini from "../../assets/dalco-959-mini-bottom.jpeg";
+import HE2000MiniSE from "../../assets/2000-mini-se.webp";
+import RamaM60Tank from "../../assets/rama-m60-tank.jpeg";
+
+export const carouselData = [
+  {
+    name: "Dalco 959 Mini",
+    caption: "A high-end Porsche 959 inspired 60",
+    img: Dalco959Mini,
+  },
+  {
+    name: "2000 Mini SE",
+    caption: "A premium 60% inspired by a piece of JDM automotive history",
+    img: HE2000MiniSE,
+  },
+  {
+    name: "RAMA WORKS M60-A TANK",
+    caption: "Limited edition M60-A made of full brass",
+    img: RamaM60Tank,
+  },
+];
