3f812886b78bd1881a0a53ccab026a9066592f7a
William Zheng <williamzheng2001@gmail.com>
2022-10-20 21:37:23 -0400

    Render product carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import ProductCarousel from "./components/ProductCarousel/ProductCarousel";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductCarousel />
     </div>
   );
 }
