a782fd672f8f162a7588693ef36cf60e4e5587d3
William Zheng <williamzheng2001@gmail.com>
2022-10-23 17:54:28 -0400

    Test product caption component

---
+++ src/components/ProductCaption/ProductCaption.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import ProductCaption from "./ProductCaption";
+
+const fakeProduct = {
+  name: "foo",
+  caption: "bar",
+};
+
+describe("product caption", () => {
+  test("renders caption", () => {
+    render(<ProductCaption product={fakeProduct} />);
+
+    const productName = screen.getByRole("heading", { name: "foo" });
+    const productDesc = screen.getByText("bar");
+    const buyBtn = screen.getByRole("button", { name: "BUY NOW" });
+
+    expect(productName).toBeInTheDocument();
+    expect(productDesc).toBeInTheDocument();
+    expect(buyBtn).toBeInTheDocument();
+  });
+});
