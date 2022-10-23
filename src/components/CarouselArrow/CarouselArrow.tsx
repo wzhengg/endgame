441f2695509d7b55eb4584c7c319422c421d6544
William Zheng <williamzheng2001@gmail.com>
2022-10-23 17:55:22 -0400

    Create carousel arrow component

---
+++ src/components/CarouselArrow/CarouselArrow.tsx
@@ -0,0 +1,21 @@
+import ChevronLeft from "../../assets/chevron-left.svg";
+import ChevronRight from "../../assets/chevron-right.svg";
+
+type ArrowProps = {
+  onClick: () => void;
+  direction: "left" | "right";
+};
+
+const CarouselArrow = ({ onClick, direction }: ArrowProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`absolute top-1/2 translate-y-[-50%] ${direction}-4 w-14 h-14`}
+    >
+      <img src={direction === "left" ? ChevronLeft : ChevronRight} alt="" />
+    </button>
+  );
+};
+
+export default CarouselArrow;
