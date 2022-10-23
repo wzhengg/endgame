bb9378e2f146f2c0b3bec6e42bf161ebfd85bfd0
William Zheng <williamzheng2001@gmail.com>
2022-10-23 17:55:59 -0400

    Update tests

--- src/components/ProductCarousel/ProductCarousel.test.tsx
+++ src/components/ProductCarousel/ProductCarousel.test.tsx
@@ -1,42 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import ProductCarousel from "./ProductCarousel";
+
+jest.mock("../ProductCaption/ProductCaption", () => () => (
+  <div data-testid="product-caption" />
+));
+
+jest.mock("../CarouselArrow/CarouselArrow", () => () => (
+  <button type="button" data-testid="arrow-btns" />
+));
 
 describe("featured products carousel", () => {
   test("renders product carousel", () => {
     render(<ProductCarousel />);
 
     const productImg = screen.getByTestId("product-img");
-    const productName = screen.getByRole("heading");
     const productCaption = screen.getByTestId("product-caption");
-    const buyBtn = screen.getByRole("button", { name: "BUY NOW" });
-    const prevBtn = screen.getByTestId("prev-btn");
-    const nextBtn = screen.getByTestId("next-btn");
+    const btns = screen.getAllByTestId("arrow-btns");
     const indicators = screen.getByTestId("indicators");
 
     expect(productImg).toBeInTheDocument();
-    expect(productName).toBeInTheDocument();
     expect(productCaption).toBeInTheDocument();
-    expect(buyBtn).toBeInTheDocument();
-    expect(prevBtn).toBeInTheDocument();
-    expect(nextBtn).toBeInTheDocument();
+    expect(btns).toHaveLength(2);
     expect(indicators).toBeInTheDocument();
-  });
-
-  test("changes product when prev and next buttons are clicked", () => {
-    render(<ProductCarousel />);
-
-    const prevBtn = screen.getByTestId("prev-btn");
-    const nextBtn = screen.getByTestId("next-btn");
-
-    const productImg = screen.getByTestId("product-img") as HTMLImageElement;
-    const src = productImg.src;
-
-    userEvent.click(prevBtn);
-    expect(src).not.toEqual(productImg.src);
-
-    userEvent.click(nextBtn);
-    userEvent.click(nextBtn);
-    expect(src).not.toEqual(productImg.src);
   });
 });
