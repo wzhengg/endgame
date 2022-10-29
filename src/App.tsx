33fb6536b5f18ebeb3597931a9a2784126da64bd
William Zheng <williamzheng2001@gmail.com>
2022-10-29 15:28:20 -0400

    Render product grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import Header from "./components/Header/Header";
 import ProductCarousel from "./components/carousel/ProductCarousel/ProductCarousel";
 import "./App.scss";
 import { carouselData } from "./components/carousel/CarouselData";
+import ProductGrid from "./components/ProductGrid/ProductGrid";
+import { products } from "./FeaturedProducts";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ProductCarousel data={carouselData} />
+      <ProductGrid data={products} />
     </div>
   );
 }
