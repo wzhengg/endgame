aca936c0106f1dee47a26de518cc7cacb3b0885b
William Zheng <williamzheng2001@gmail.com>
2022-10-29 18:17:27 -0400

    Test product card component

---
+++ src/components/product-grid/ProductCard/ProductCard.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import ProductCard from "./ProductCard";
+
+const fakeProduct = {
+  name: "foo",
+  price: 5.0,
+  imgs: [""],
+};
+
+describe("product card", () => {
+  test("renders product card", () => {
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    render(<ProductCard product={fakeProduct} formatter={formatter} />);
+
+    const productName = screen.getByRole("heading", { name: "FOO" });
+    const productPrice = screen.getByText("$5.00");
+    const productImg = screen.getByRole("img");
+
+    expect(productName).toBeInTheDocument();
+    expect(productPrice).toBeInTheDocument();
+    expect(productImg).toBeInTheDocument();
+  });
+});
