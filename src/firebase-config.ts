a5aa175465f9f485cd22ad612458144edf7a9f6b
William Zheng <williamzheng2001@gmail.com>
2022-11-19 20:30:49 -0500

    Initialize firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTok1GrRiGa55W5E83ZNRmlzWUaVbYkQE",
+  authDomain: "endgame-5e9ac.firebaseapp.com",
+  projectId: "endgame-5e9ac",
+  storageBucket: "endgame-5e9ac.appspot.com",
+  messagingSenderId: "685832735318",
+  appId: "1:685832735318:web:31a7a9675e35fdb4d589f7",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
