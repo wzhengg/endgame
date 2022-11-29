a451a884635244587988964cf64c04bc74cacee5
William Zheng <williamzheng2001@gmail.com>
2022-11-28 19:00:57 -0500

    Get all products

--- src/components/ProductsLayout/ProductsLayout.tsx
+++ src/components/ProductsLayout/ProductsLayout.tsx
@@ -2,15 +2,16 @@ import { useState, useEffect, useMemo } from "react";
 import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
 import { db } from "../../firebase-config";
 
-type Keyboard = {
+type Product = {
   id: string;
   name: string;
   price: number;
+  category: string;
   images: string[];
 };
 
 const ProductsLayout = () => {
-  const [keyboards, setKeyboards] = useState<Keyboard[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const formatter = useMemo(
     () =>
@@ -18,43 +19,48 @@ const ProductsLayout = () => {
     []
   );
 
-  const getKeyboards = async () => {
+  const getProducts = async () => {
     try {
-      const newKeyboards: Keyboard[] = [];
+      const newProducts: Product[] = [];
       const querySnapshot = (await getDocs(
-        collection(db, "keyboards")
-      )) as QuerySnapshot<{ name: string; price: number; images: string[] }>;
+        collection(db, "products")
+      )) as QuerySnapshot<{
+        name: string;
+        price: number;
+        category: string;
+        images: string[];
+      }>;
       querySnapshot.forEach((doc) => {
-        newKeyboards.push({ id: doc.id, ...doc.data() });
+        newProducts.push({ id: doc.id, ...doc.data() });
       });
-      setKeyboards(newKeyboards);
+      setProducts(newProducts);
     } catch (err) {
       console.error(err);
     }
   };
 
   useEffect(() => {
-    getKeyboards();
+    getProducts();
   }, []);
 
   return (
     <div className="bg-gray-100 p-20">
       <div className="grid grid-cols-5 gap-6">
-        {keyboards.map((keyboard) => (
+        {products.map((product) => (
           <div
-            key={keyboard.id}
+            key={product.id}
             className="flex flex-col items-center gap-3 pb-3 shadow-md rounded-md overflow-hidden"
           >
             <img
-              src={keyboard.images[0]}
+              src={product.images[0]}
               alt=""
               className="object-cover w-full h-52"
             />
             <h3 className="font-semibold tracking-widest text-gray-600">
-              {keyboard.name.toUpperCase()}
+              {product.name.toUpperCase()}
             </h3>
             <p className="text-sm tracking-wider text-gray-600">
-              {formatter.format(keyboard.price)}
+              {formatter.format(product.price)}
             </p>
           </div>
         ))}
