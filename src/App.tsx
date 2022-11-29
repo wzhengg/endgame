367e7d7c774847c539f1e3d027eef01c34e54058
William Zheng <williamzheng2001@gmail.com>
2022-11-28 22:16:08 -0500

    Get featured collections data from Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header";
 import ProductCarousel from "./components/carousel/ProductCarousel/ProductCarousel";
 import "./App.scss";
 import ProductGrid from "./components/product-grid/ProductGrid/ProductGrid";
-import { products } from "./components/product-grid/FeaturedProducts";
 import Footer from "./components/Footer/Footer";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase-config";
@@ -17,8 +16,19 @@ class CarouselObject {
   ) {}
 }
 
+class CollectionObject {
+  constructor(
+    readonly id: string,
+    readonly category: string,
+    readonly products: { name: string; price: number; images: string[] }[]
+  ) {}
+}
+
 function App() {
   const [carouselData, setCarouselData] = useState<CarouselObject[]>([]);
+  const [collectionsData, setCollectionsData] = useState<CollectionObject[]>(
+    []
+  );
 
   const getCarouselData = async () => {
     try {
@@ -36,15 +46,35 @@ function App() {
     }
   };
 
+  const getFeaturedCollections = async () => {
+    try {
+      const newCollectionsData: CollectionObject[] = [];
+      const querySnapshot = await getDocs(
+        collection(db, "featured-collections")
+      );
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        const data = doc.data();
+        newCollectionsData.push(
+          new CollectionObject(doc.id, data.category, data.products)
+        );
+      });
+      setCollectionsData(newCollectionsData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getCarouselData();
+    getFeaturedCollections();
   }, []);
 
   return (
     <div className="App">
       <Header />
       <ProductCarousel data={carouselData} />
-      <ProductGrid data={products} />
+      <ProductGrid data={collectionsData} />
       <Footer />
     </div>
   );
