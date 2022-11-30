629e9618cdc1dbd8ed78240f4bc7fc261adb2301
William Zheng <williamzheng2001@gmail.com>
2022-11-29 21:12:23 -0500

    Render product cards using existing component

--- src/components/ProductsLayout/ProductsLayout.tsx
+++ src/components/ProductsLayout/ProductsLayout.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useMemo } from "react";
 import { Product, getProducts } from "../../api/products";
+import ProductCard from "../product-grid/ProductCard/ProductCard";
 
 const ProductsLayout = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -18,22 +19,11 @@ const ProductsLayout = () => {
     <div className="bg-gray-100 p-20">
       <div className="grid grid-cols-5 gap-6">
         {products.map((product) => (
-          <div
+          <ProductCard
+            product={product}
+            formatter={formatter}
             key={product.id}
-            className="flex flex-col items-center gap-3 pb-3 shadow-md rounded-md overflow-hidden"
-          >
-            <img
-              src={product.images[0]}
-              alt=""
-              className="object-cover w-full h-52"
-            />
-            <h3 className="font-semibold tracking-widest text-gray-600">
-              {product.name.toUpperCase()}
-            </h3>
-            <p className="text-sm tracking-wider text-gray-600">
-              {formatter.format(product.price)}
-            </p>
-          </div>
+          />
         ))}
       </div>
     </div>
