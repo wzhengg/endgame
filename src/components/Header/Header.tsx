e65ac05b09813bd6e87e20c735fd441f592e39df
William Zheng <williamzheng2001@gmail.com>
2022-12-09 21:05:15 -0500

    Link root route and products route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
 import AccountIcon from "../../assets/icons/account-outline.svg";
 import CartIcon from "../../assets/icons/cart-outline.svg";
 
 const Header = () => {
   return (
     <nav className="flex justify-between items-center px-9">
-      <h1 className="text-xl font-semibold tracking-widest">ENDGAME</h1>
+      <h1 className="text-xl font-semibold tracking-widest">
+        <Link to={"/"}>ENDGAME</Link>
+      </h1>
       <ul className="flex gap-6">
-        <li className="py-4 px-2 font-light tracking-wider">PRODUCTS</li>
+        <li className="py-4 px-2 font-light tracking-wider">
+          <Link to={"products"}>PRODUCTS</Link>
+        </li>
         <li className="py-4 px-2 font-light tracking-wider">UPDATES</li>
         <li className="py-4 px-2 font-light tracking-wider">SOCIALS</li>
         <li className="py-4 px-2 font-light tracking-wider">ABOUT US</li>
