f5356ab82a59c7f09ba4788869f7710349e032c6
William Zheng <williamzheng2001@gmail.com>
2022-12-09 20:48:55 -0500

    Create error page component

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,8 @@
+const ErrorPage = () => (
+  <div className="tracking-widest text-gray-700 h-screen flex flex-col justify-center items-center">
+    <h1 className="text-4xl font-bold mb-16">OOPS!</h1>
+    <p>SORRY, AN UNEXPECTED ERROR HAS OCCURRED.</p>
+  </div>
+);
+
+export default ErrorPage;
