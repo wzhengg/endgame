ce1468e533855b88fb99c5bee7cdc221de333668
William Zheng <williamzheng2001@gmail.com>
2022-12-11 20:50:13 -0500

    Add links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,20 @@ const Header = () => {
   return (
     <nav className="flex justify-between items-center px-9">
       <h1 className="text-xl font-semibold tracking-widest">
-        <Link to={"/"}>ENDGAME</Link>
+        <Link to="/">ENDGAME</Link>
       </h1>
       <ul className="flex gap-6">
         <li className="py-4 px-2 font-light tracking-wider">
-          <Link to={"products"}>PRODUCTS</Link>
+          <Link to="products">PRODUCTS</Link>
         </li>
         <li className="py-4 px-2 font-light tracking-wider">UPDATES</li>
         <li className="py-4 px-2 font-light tracking-wider">SOCIALS</li>
         <li className="py-4 px-2 font-light tracking-wider">ABOUT US</li>
       </ul>
       <div className="flex gap-3">
-        <img src={AccountIcon} alt="" className="w-6" />
+        <Link to="login">
+          <img src={AccountIcon} alt="" className="w-6" />
+        </Link>
         <img src={CartIcon} alt="" className="w-6" />
       </div>
     </nav>
