be0f1a8b8cf87ebfc7c5e675c0bd946ef287dcca
William Zheng <williamzheng2001@gmail.com>
2022-12-22 12:58:15 -0500

    Add remove button

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -5,9 +5,20 @@ const CartPage = () => {
 
   return (
     <div>
-      {cart.map((item, i) => (
-        <div key={i}>
-          <div>{item.product.name}</div>
+      {cart.map((item) => (
+        <div key={item.product.id}>
+          {item.product.name}, {item.quantity}
+          <button
+            type="button"
+            onClick={() => {
+              const newCart = cart.filter(
+                (obj) => obj.product.id !== item.product.id
+              );
+              setCart(newCart);
+            }}
+          >
+            REMOVE
+          </button>
         </div>
       ))}
     </div>
