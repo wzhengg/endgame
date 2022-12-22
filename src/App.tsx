9e9ab635230d9a67075e1e9a40d832fc87fec078
William Zheng <williamzheng2001@gmail.com>
2022-12-22 11:40:36 -0500

    Create cart state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.scss";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Product } from "./api/products";
+
+export type CartItem = {
+  product: Product;
+  quantity: number;
+};
+
+type CartContext = {
+  cart: CartItem[];
+  setCart: Dispatch<SetStateAction<CartItem[]>>;
+};
 
 function App() {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
   return (
     <div className="App min-h-screen flex flex-col">
       <Header />
-      <Outlet />
+      <Outlet context={{ cart, setCart }} />
       <Footer />
     </div>
   );
+}
+
+export function useCart() {
+  return useOutletContext<CartContext>();
 }
 
 export default App;
